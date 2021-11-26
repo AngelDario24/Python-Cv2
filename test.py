14d3c696e4c25c77d7c5658d75fd67d9f4f66a3f
Angel Dario Santana Castillo <89232478+AngelDario24@users.noreply.github.com>
2021-11-26 10:50:07 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+import pytesseract as tess #importando Pytesseract
+import cv2
+tess.pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe' #Corregir el problema de path
+
+#Ruta de imagen
+
+Ruta = cv2.imread('C:\\Users\\Tony\\Desktop\\Prueba\\texto2.jpg')
+
+Texto = tess.image_to_string(Ruta) #conversion para mostrar el texto de la imagen
+
+cv2.imshow("Pruebas",Ruta)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print(Texto)
